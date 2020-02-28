8df97d5b52b6c8db745cb980af3262c75c2b4a38
Igor <i.shtyka@gmail.com>
2020-02-28 14:17:59 +0200

    Новый коммит

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+"esversion: 6";
+
+// let money = prompt('Ваш бюджет на месяц?', ''),
+//     time = prompt('Введите дату в формате YYYY-MM-DD', '')
+//     income,
+//     timeData,
+//     savings;   
+
+//     firstExpence = prompt('Введите обязательную статью расходов в этом месяце', '');
+//     secondExpence = prompt('Во сколько обойдется?', '');
+
+// let expenses = {
+//       firstExpence:secondExpence
+//     };
+
+// let appData = {
+//     budget: money,
+//     timeData: time,
+//     expenses:
+//     optionalExpenses,
+//     income,
+//     savings: false;
+// }
+let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <60);
+
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+let calc = (a,b) => {a+b;};
+console.log(calc(4,3));
