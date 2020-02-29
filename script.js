bd5625ae73fc2265a14a3affd6be94d79aafe794
Igor <i.shtyka@gmail.com>
2020-02-29 16:58:49 +0200

    Первое задание

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-'use strict';
-"esversion: 6";
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+    
 
-// let money = prompt('Ваш бюджет на месяц?', ''),
-//     time = prompt('Введите дату в формате YYYY-MM-DD', '')
-//     income,
-//     timeData,
-//     savings;   
-
-//     firstExpence = prompt('Введите обязательную статью расходов в этом месяце', '');
-//     secondExpence = prompt('Во сколько обойдется?', '');
-
-// let expenses = {
-//       firstExpence:secondExpence
-//     };
-
-// let appData = {
-//     budget: money,
-//     timeData: time,
-//     expenses:
-//     optionalExpenses,
-//     income,
-//     savings: false;
-// }
-let num = 50;
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <60);
-
-// for (let i = 1; i < 8; i++) {
-//     if (i == 6) {
-//         continue;
-//     }
-//     console.log(i);
-// }
-let calc = (a,b) => {a+b;};
-console.log(calc(4,3));
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        expensesNeeded : prompt("Введите обязательную статью расходов в этом месяце", ""), 
+        expensesCosts : prompt("Во сколько обойдется?", "")
+    },
+    optionalExpenses: {
+        opExpenses: " "
+    },
+    income: [],
+    savings: false
+};
+alert("Бюджет на день " + money/30);
